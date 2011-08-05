6c16b8a69c64cf3a387b908c4635a05762c9092b
pecet1@gmail.com <pecet1@gmail.com@49f10a27-98c2-4d3b-bd2e-2f095fc3f814>
2011-08-05 21:12:40 +0000

    support for more than one configuration in INI file (you can download pics from different subreddits do different directories) also switched from Config::Simple to Config::Tiny

--- trunk/redditowy_ssacz/ssacz.pl
+++ trunk/redditowy_ssacz/ssacz.pl
@@ -12,29 +12,13 @@ use strict;
 use warnings;
 use JSON;
 use LWP::Simple;
-use Config::Simple;
+use Config::Tiny;
 use Data::Dumper; # for debug only
-
-#config
-my $cfg = new Config::Simple('./ssacz.ini');
-
-my $save_dir = $cfg->get_block('gonewild')->{'save_dir'};
-my $input_url = $cfg->get_block('gonewild')->{'input_url'};
-my $numpages = $cfg->get_block('gonewild')->{'numpages'};
-my $filter = $cfg->get_block('gonewild')->{'filter'};
-
-#the rest variables
-my $url = $input_url;
-my $after;
-
-my $content;
-my $parsed;
-
 
 #subs
 sub download_img
 {
-	my ($img_url, $filename, $selfpost) = @_;
+	my ($save_dir, $img_url, $filename, $selfpost) = @_;
 	my $s = '';
 	$s = '(from selfpost)' if($selfpost);
 	
@@ -49,52 +33,74 @@ sub download_img
 	}	
 }
 
+#config
+my $cfg = new Config::Tiny;
+$cfg = $cfg->read('./ssacz.ini');
 
 #main
-for my $page (1..$numpages)	
+for my $subreddit (keys %$cfg)
 {
-	$content = get($url);
-	unless (defined $content) {
-	    say "Error getting URL $url";
-	    next;
-	}	
-	$parsed = from_json($content);
-	say "Downloading page $page of $numpages ($url)";
+	say "------ Downloading using configuration \"$subreddit\" -----";
 	
-	$parsed = $parsed->{'data'}->{'children'}; 
+	my $save_dir = $cfg->{$subreddit}->{'save_dir'};
+	my $input_url = $cfg->{$subreddit}->{'input_url'};
+	my $numpages = $cfg->{$subreddit}->{'numpages'};
+	my $filter = $cfg->{$subreddit}->{'filter'};
 	
-	for my $i (@$parsed)
+	#the rest variables
+	my $url = $input_url;
+	my $after;
+	
+	my $content;
+	my $parsed;	
+	
+	# get one subreddit at the time
+	for my $page (1..$numpages)	
 	{
-	   $i = $i->{'data'};
-
-	   if($i->{'title'} =~ m/$filter/i) 
-	   {
-		   if($i->{'selftext'})		   
-		   {	   	
-		   		say "-- Got selftext post";
-		   		my @img_urls = $i->{'selftext'} =~ m|http://(?:i\.)?(?:imgur\.com/([a-zA-Z0-9]*\.jpg))|g;
-		   		#say Dumper(\@img_urls);
-				for my $filename (@img_urls)
-				{
-					my $img_url = "http://i.imgur.com/$filename";
-					download_img($img_url, $filename, 1);				
-				}		   	
-
-		   }
-		   else
-		   {
-		   	   if ($i->{'url'} =~ m|http://(?:i\.)?(imgur\.com/([a-zA-Z0-9]*\.jpg))|)
-			   {
-				say "-- Got single image post";				   	
-			   		my $img_url = "http://i.$1";
-			   		my $filename = $2;
-					download_img($img_url, $filename, 0);
-			   }
-		   }
-		}
-		$after = $i->{'id'};	   
-	}
+		$content = get($url);
+		unless (defined $content) {
+		    say "Error getting URL $url, skipping.";
+		    next;
+		}	
+		$parsed = from_json($content);
+		say "Downloading page $page of $numpages ($url)";
+		
+		$parsed = $parsed->{'data'}->{'children'}; 
+		
+		for my $i (@$parsed)
+		{
+		   $i = $i->{'data'};
 	
-	$url = "$input_url&after=t3_$after";
+		   if(!$filter or $i->{'title'} =~ m/$filter/i) 
+		   {
+			   if($i->{'selftext'})		   
+			   {	   	
+			   		say "-- Got selftext post";
+			   		my @img_urls = $i->{'selftext'} =~ m|http://(?:i\.)?(?:imgur\.com/([a-zA-Z0-9]*\.jpg))|g;
+			   		#say Dumper(\@img_urls);
+					for my $filename (@img_urls)
+					{
+						my $img_url = "http://i.imgur.com/$filename";
+						download_img($save_dir, $img_url, $filename, 1);				
+					}		   	
+	
+			   }
+			   else
+			   {
+			   	   if ($i->{'url'} =~ m|http://(?:i\.)?(imgur\.com/([a-zA-Z0-9]*\.jpg))|)
+				   {
+					say "-- Got single image post";				   	
+				   		my $img_url = "http://i.$1";
+				   		my $filename = $2;
+						download_img($save_dir, $img_url, $filename, 0);
+				   }
+			   }
+			}
+			$after = $i->{'id'};	   
+		}
+		
+		$url = "$input_url&after=t3_$after";
+	}
+	say "Done with $subreddit!";
 }
-say 'Done!';
+say 'All done!';
